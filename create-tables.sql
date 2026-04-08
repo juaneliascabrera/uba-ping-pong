589d33c07f5baaf5965944704cd69bd405ee720b
Juan Elias Cabrera <jecabrera@dc.uba.ar>
2026-04-08 15:49:08 -0300

    SQL Querys to create DB and Tables.

--- create-tables.sql
+++ create-tables.sql
@@ -7,22 +7,22 @@ create schema ping_pong_sport;
 grant usage on schema ping_pong_sport to ping_pong_admin;
 
 -- Tabla de Productos
-create table terox.productos (
-    producto_id serial primary key,
-    nombre varchar(100) not null,
-    precio int not null check (precio >= 0),
-    stock int not null check (stock >= 0),
-    descripcion text,
-    imagen_url varchar(255)
+create table ping_pong_sport.matches (
+    match_id serial primary key,
+    player_1 varchar(100) not null,
+    player_2 varchar(100) not null,
+    played_at timestamptz default current_timestamp
+--    comentarios text,
 --    usuario_id REFERENCES terox.usuarios(id) -- FK a usuarios, para relacionar un producto/post con su publicador
 );
 
-CREATE TABLE terox.usuarios (
-    id SERIAL PRIMARY KEY,
-    username TEXT UNIQUE NOT NULL,
-    password_hash TEXT NOT NULL,
-    nombre TEXT NOT NULL,
-    email TEXT UNIQUE NOT NULL
+-- 2. Creamos el "hijo" que depende del padre
+CREATE TABLE ping_pong_sport.sets (
+    set_id SERIAL PRIMARY KEY,
+    match_id INT REFERENCES matches(match_id) ON DELETE CASCADE, -- Esta es la FK
+    points_player_1 INT check (points_player_1 >= 0),
+    points_player_2 INT check (points_player_2 >= 0),
+    set_number INT check (set_number >= 0) -- Para saber si es el set 1, 2 o 3
 );
 
 grant select, insert, update, delete on terox.usuarios to terox_admin;
