f215cea7e5003ae23a5dcd3c43b3a92eb9ee0204
Juan Elias Cabrera <jecabrera@dc.uba.ar>
2026-04-16 18:27:08 -0300

    Best of input added.
    Co-authored-by: Mario Livia <marioaleliv@hotmail.com>

--- src/scripts/load_match.ts
+++ src/scripts/load_match.ts
@@ -8,6 +8,20 @@ function formToDict(form: HTMLFormElement) {
     return data;
 }
 
+function validSets(sets: any[]) {
+    // First we'll ensure that sets length is odd.
+    if (sets.length % 2 != 1) {
+        return false;
+    }
+    //Now we'll ensure every set has been won by 2 points of difference or more.
+    for (let set of sets) {
+        if (set['player_1_points'] - set['player_2_points'] < 2) {
+            return false;
+        }
+
+    }
+
+}
 
 async function addMatch(url: string) {
     const form: HTMLFormElement = document.getElementById("load_match") as HTMLFormElement;
@@ -19,13 +33,17 @@ async function addMatch(url: string) {
         let set_number = 1;
 
         const sets = [];
-        
-        for(set_number = 1; set_number <= 3; set_number++){
+
+        for (set_number = 1; set_number <= 3; set_number++) {
             let current_set_p1 = `set_${set_number}_p1`;
             let current_set_p2 = `set_${set_number}_p2`;
             if (data[current_set_p1] && data[current_set_p2]) {
                 sets.push({ points_player_1: Number(data[current_set_p1]), points_player_2: Number(data[current_set_p2]) });
             }
+        }
+        if (!validSets(sets)) {
+            alert('Invalid sets')
+            return;
         }
         const formatted_data = {
             player_1_username: data.player_1_username,
@@ -39,7 +57,7 @@ async function addMatch(url: string) {
                 method: "POST",
                 headers: { "Content-Type": "application/json" },
                 body: JSON.stringify(formatted_data)
-                
+
             });
 
             if (response.ok) {
